c7bff8f91827b90aa7d0587ce66dec3d4cc7a079
cookieblues <cookieblues@outlook.dk>
2023-05-23 16:05:06 +0200

    changed job

--- blog/tasks.py
+++ blog/tasks.py
@@ -2,17 +2,10 @@ from celery.schedules import crontab
 from celery.task import periodic_task
 from celery.utils.log import get_task_logger
 
-from blog.models import Post
-
 logger = get_task_logger(__name__)
 
 
 @periodic_task(run_every=(crontab()), name="random_test", ignore_result=True)
 def add_test():
-    new_post = Post(
-        title="test",
-        body="test body",
-        featured_image=None,
-        published=True,
-    )
-    Post.objects.create(new_post)
+    logger.debug("test job")
+    print("test jobby")
